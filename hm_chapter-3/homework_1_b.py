44cb654c90525f3ec7e646a94069c7537b735fb6
KhantsevaAA <3104nastya@gmail.com>
2022-11-17 16:29:26 +0300

    edit hm3 1a and 1b

--- hm_chapter-3/homework_1_b.py
+++ hm_chapter-3/homework_1_b.py
@@ -1,9 +1,9 @@
-class Binary:
+class Ternary:
     def __init__(self, num):  # инициализация объекта Binary (num - число в 10 сс)
         self.num = 0
         n = 0
         while num != 0:
-            self.num += ((num % 10) * (2 ** n))
+            self.num += ((num % 10) * (3 ** n))
             num = num // 10
             n += 1
 
@@ -27,16 +27,16 @@ def translation(number):
     n = 0
     output = 0
     while number != 0:
-        output = (number % 2) * (10 ** n) + output
-        number = number // 2
+        output = (number % 3) * (10 ** n) + output
+        number = number // 3
         n += 1
     return output
 
 
-x = int(input("Введите первое число в двоичной системемe счисления: "))
-y = int(input("Введите второе число в двоичной системемe счисления: "))
-a = Binary(x)
-b = Binary(y)
+x = int(input("Введите первое число в троичной системемe счисления: "))
+y = int(input("Введите второе число в троичной системемe счисления: "))
+a = Ternary(x)
+b = Ternary(y)
 print(a + b)
 print(a - b)
 print(a * b)
