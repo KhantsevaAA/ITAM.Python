502bb1231349068ebab462ca458e39d1f1c95080
KhantsevaAA <3104nastya@gmail.com>
2022-11-18 19:30:07 +0300

    edit hm_test_2a.py

--- hm_chapter-3/hm_test_2A.py
+++ hm_chapter-3/hm_test_2A.py
@@ -1,18 +1,34 @@
 class NumerialSystem:
     def __init__(self, n):
-        mas = [0] * n
-        ss = [0] * n
-        self.str = ''
+        self.mas = [0] * n
+        self.ss = [0] * n
+
         print("Введите систему численя «BINARY» и «TERNARY», а дальше само число в заданной системе счисления.")
         for i in range(n):
             c = input().split(' ')
             # print(f"{c[0]} {c[1]}")
-            mas[i] = int(c[1])
+            self.mas[i] = int(c[1])
             if c[0] == "BINARY":
-                ss[i] = binary(mas[i])
+                self.ss[i] = binary(self.mas[i])
             elif c[0] == "TERNARY":
-                ss[i] = termary(mas[i])
-            print(ss[i])
+                self.ss[i] = termary(self.mas[i])
+            # self.strm += f"{mas[i]}  "
+            # self.strs += f"{ss[i]}  "
+
+    def num_sort(self):
+        for i in range(len(self.ss) - 1):
+            for j in range(len(self.ss) - i - 1):
+                if self.ss[j] > self.ss[j + 1]:
+                    c = self.ss[j]
+                    self.ss[j] = self.ss[j + 1]
+                    self.ss[j + 1] = c
+                    c = self.mas[j]
+                    self.mas[j] = self.mas[j + 1]
+                    self.mas[j + 1] = c
+        print(self.mas)
+
+    def __str__(self):  # конвертирование объекта Binary в строку
+        return str(self)
 
 
 def binary(num):
@@ -37,3 +53,4 @@ def termary(num):
 
 n = int(input("Введите кол-во чисел: "))
 str = NumerialSystem(n)
+str.num_sort()
